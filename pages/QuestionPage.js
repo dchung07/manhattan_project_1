a015d35ae909fceac350db2a6f5436bb3d30ddae
zipianli818 <zipian.li818@gmail.com>
2023-11-18 22:35:43 +1100

    hi

---
+++ pages/QuestionPage.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <Script>
+            {
+                
+            }
+        </Script>
+    </head>
+
+    <body>
+
+    <h2>Interaction Quiz Game</h2>
+
+    <button type="button" onclick="">Option1</button>
+
+    </body>
+</html>
