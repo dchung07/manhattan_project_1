ff07227e1dc0d3cc9801de474981b796cba1b7f3
zipianli818 <zipian.li818@gmail.com>
2023-11-18 22:35:31 +1100

    hi

---
+++ pages/FirstPage.js
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <Script>
+            function startPlay() {
+                
+            }
+        </Script>
+    </head>
+
+    <body>
+
+    <h2>Interaction Quiz Game</h2>
+
+    <button type="button" onclick="startPlay()">Click Me To Play!</button>
+
+    </body>
+</html>
