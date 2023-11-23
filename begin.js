9098f065a7d038dffb5eb92851b3a4c0bbc93c68
zipianli818 <zipian.li818@gmail.com>
2023-11-23 16:30:56 +1100

    fix typo and add event

--- begin.js
+++ begin.js
@@ -1,3 +1,6 @@
-const Title = document.getElementById("Title");
-const Start = socument.getElementById("Start");
+const Title = document.getElementById("title");  // Corrected the ID
+const Start = document.getElementById("Start");  // Corrected the ID
 
+Start.addEventListener("click", function() {
+    window.location.href = 'question_page.html';
+});
